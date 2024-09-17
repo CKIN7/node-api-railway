6db95be24eacd290f525f96f6a18afc4c135edac
CKIN7 <covain77@gmail.com>
2024-09-16 22:05:48 -0400

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,24 +1,8 @@
 import cors from 'cors';
 
-const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:1234',
-    'https://movies.com',
-    'https://midu.dev',
-    'http://localhost:5173/men',
-];
-
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+export const corsMiddleware = () =>
     cors({
-        origin: (origin, callback) => {
-            if (acceptedOrigins.includes(origin)) {
-                return callback(null, true);
-            }
-
-            if (!origin) {
-                return callback(null, true);
-            }
-
-            return callback(new Error('Not allowed by CORS'));
-        },
+        origin: '*',
+        methods: 'GET, POST, PUT, DELETE, OPTIONS',
+        allowedHeaders: 'Content-Type, Accept',
     });
